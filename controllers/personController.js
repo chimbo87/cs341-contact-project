cc59e8b30fdba98f644ac1bfd25629c836d19c5e
Archford Nhanga <archfordnhanga@gmail.com>
2025-05-09 13:16:53 +0200

    controller files

---
+++ controllers/personController.js
@@ -0,0 +1,93 @@
+
+const Person = require('../models/Person');
+
+// Get all people
+const getAllPeople = async (req, res) => {
+  try {
+    const people = await Person.find();
+    res.json(people);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Get one person
+const getPerson = async (req, res) => {
+  try {
+    const person = await Person.findById(req.params.id);
+    if (!person) {
+      return res.status(404).json({ message: 'Cannot find person' });
+    }
+    res.json(person);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Create a person
+const createPerson = async (req, res) => {
+  const person = new Person({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  });
+
+  try {
+    const newPerson = await person.save();
+    res.status(201).json(newPerson);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Update a person
+const updatePerson = async (req, res) => {
+  try {
+    const person = await Person.findById(req.params.id);
+    if (!person) {
+      return res.status(404).json({ message: 'Cannot find person' });
+    }
+
+    if (req.body.firstName != null) person.firstName = req.body.firstName;
+    if (req.body.lastName != null) person.lastName = req.body.lastName;
+    if (req.body.email != null) person.email = req.body.email;
+    if (req.body.favoriteColor != null) person.favoriteColor = req.body.favoriteColor;
+    if (req.body.birthday != null) person.birthday = req.body.birthday;
+
+    const updatedPerson = await person.save();
+    res.json(updatedPerson);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Delete a person
+const deletePerson = async (req, res) => {
+  try {
+    const person = await Person.findById(req.params.id);
+    if (!person) {
+      return res.status(404).json({ message: 'Cannot find person' });
+    }
+
+    await person.deleteOne();
+    res.json({ 
+      message: 'Person deleted successfully',
+      deletedPerson: person
+    });
+  } catch (err) {
+    res.status(500).json({ 
+      message: 'Failed to delete person',
+      error: err.message 
+    });
+  }
+};
+
+module.exports = {
+  getAllPeople,
+  getPerson,
+  createPerson,
+  updatePerson,
+  deletePerson
+};
--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,91 +1,27 @@
+// routes/personRoutes.js
 const express = require('express');
 const router = express.Router();
-const Person = require('../models/Person');
+const {
+  getAllPeople,
+  getPerson,
+  createPerson,
+  updatePerson,
+  deletePerson
+} = require('../controllers/personController');
 
 // GET all people
-router.get('/', async (req, res) => {
-  try {
-    const people = await Person.find();
-    res.json(people);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get('/', getAllPeople);
 
 // GET one person
-router.get('/:id', getPerson, (req, res) => {
-  res.json(res.person);
-});
+router.get('/:id', getPerson);
 
 // POST create a person
-router.post('/', async (req, res) => {
-  const person = new Person({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  });
-
-  try {
-    const newPerson = await person.save();
-    res.status(201).json(newPerson);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+router.post('/', createPerson);
 
 // PUT update a person
-router.put('/:id', getPerson, async (req, res) => {
-  if (req.body.firstName != null) {
-    res.person.firstName = req.body.firstName;
-  }
-  if (req.body.lastName != null) {
-    res.person.lastName = req.body.lastName;
-  }
-  if (req.body.email != null) {
-    res.person.email = req.body.email;
-  }
-  if (req.body.favoriteColor != null) {
-    res.person.favoriteColor = req.body.favoriteColor;
-  }
-  if (req.body.birthday != null) {
-    res.person.birthday = req.body.birthday;
-  }
-
-  try {
-    const updatedPerson = await res.person.save();
-    res.json(updatedPerson);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+router.put('/:id', updatePerson);
 
 // DELETE a person
-
-router.delete('/:id', getPerson, async (req, res) => {
-    try {
-      await Person.deleteOne({ _id: res.person._id });
-      res.json({ message: 'Deleted Person' });
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  });
-
-// Middleware to get a person by ID
-async function getPerson(req, res, next) {
-  let person;
-  try {
-    person = await Person.findById(req.params.id);
-    if (person == null) {
-      return res.status(404).json({ message: 'Cannot find person' });
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-
-  res.person = person;
-  next();
-}
+router.delete('/:id', deletePerson);
 
 module.exports = router;
